ff4c923824065647743627b07b9d5ef80f7f44e4
Christian Bourlier <chrisbourlier@hotmail.com>
2018-10-11 22:00:07 -0600

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { ProfileComponent } from './components/profile/profile.component';
 import { ContainerComponent } from './components/container/container.component';
-import { SocialMediaComponent } from './components/social-media/social-media.component'
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     ContainerComponent,
-    SocialMediaComponent
+    SocialMediaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
