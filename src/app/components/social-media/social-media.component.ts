8de3c3f9fd14553c4f885418f5514c449f3b9012
Christian Bourlier <chrisbourlier@hotmail.com>
2018-10-11 23:08:51 -0600

    social media component created and styled

--- src/app/components/social-media/social-media.component.ts
+++ src/app/components/social-media/social-media.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+
+import { SocialMedia } from '../../models/SocialMedia'
 
 @Component({
   selector: 'app-social-media',
   templateUrl: './social-media.component.html',
   styleUrls: ['./social-media.component.css']
 })
+
 export class SocialMediaComponent implements OnInit {
+  socialMedia: SocialMedia
 
   constructor() { }
 
   ngOnInit() {
+    this.socialMedia = {
+      facebook: 'https://facebook.com/chrisbourlier',
+      github: 'https://github.com/feelgreatfoodie',
+      linkedIn: 'https://linkedin.com/in/christianbourlier/' ,
+      twitter: 'https://twitter.com/chrisbourlier'
+    }
   }
 
 }
---
+++ src/app/models/SocialMedia.ts
@@ -0,0 +1,6 @@
+export interface SocialMedia {
+  linkedIn?: string,
+  github?: string,
+  facebook?: string,
+  twitter?: string
+}
